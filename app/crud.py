479777552419f74f459fa4aeb6c95c58fa85b2dc
ik-okoro <ik.o.okoro@gmail.com>
2021-02-26 12:35:39 -0500

    Incorporates moving average code
    Includes a little bit of code cleanup too. Still quite a bit to do there.

--- app/crud.py
+++ app/crud.py
@@ -12,23 +12,24 @@ from sqlalchemy.sql.sqltypes import Float
 
 from .db_init import Members, Families
 
+today = date.today() - timedelta(days = 210)
+
 def view_member(db: Session, member_id: int):
     return db.query(Members).filter(Members.id == member_id).first()
 
 def view_family(db: Session, family_id: int):
     return db.query(Families).filter(Families.id == family_id).first()
 
-def exit_date_subset(*, db: Session, table = Members, today_delta = 178, start_range: int):
-    today = date.today() - timedelta(days = today_delta)
-    start = today - timedelta(days = start_range)
-    subset = db.query(table).filter(and_(table.date_of_exit >= start, table.date_of_exit <= today)).subquery()
+def exit_date_subset(*, db: Session, table = Members, end = today, start_range: int):
+    start = end - timedelta(days = start_range)
+    subset = db.query(table).filter(and_(table.date_of_exit >= start, table.date_of_exit <= end)).subquery()
     return subset
 
-def count_exits(db: Session, exit_type: str, time_range: int):
+def count_exits(db: Session, exit_type: str, time_range: int, stop = today):
     # today = date.today() - timedelta(days=210)
     # start = today - timedelta(days = time_range)
     # date_subset = db.query(Members).filter(and_(Members.date_of_exit >= start, Members.date_of_exit <= today)).subquery()
-    date_subset = exit_date_subset(db = db, start_range=time_range)
+    date_subset = exit_date_subset(db = db, start_range=time_range, end= stop)
     count = db.query(func.count(date_subset.c.exit_destination)).one()[0]
     exit_subset = db.query(date_subset).filter(date_subset.c.exit_destination.like(exit_type))
     exit_count = exit_subset.count()
@@ -37,12 +38,13 @@ def count_exits(db: Session, exit_type: str, time_range: int):
 def avg_stay(db: Session, time_range: int):
     date_subset = exit_date_subset(db=db, start_range= time_range)
     avg = db.query(func.avg(date_subset.c.length_of_stay)).one()[0]
-    return avg
+    return round(avg)
 
 def income_increase(db: Session, time_range: int):
     date_subset = exit_date_subset(db=db, start_range=time_range)
 
     #Filter for only those that reported income at entry
-    income = db.query(date_subset).filter(date_subset.c.demographics["income"].astext.cast(Float) != -1).subquery()
-    num_increased = db.query(income).filter(income.c.demographics["income"].astext.cast(Float) < income.c.income_at_exit)
-    return num_increased.count()
+    #   and check if exit income greater than entry's
+    income = db.query(date_subset).filter(and_(date_subset.c.demographics["income"].astext.cast(Float) != -1.0, 
+                                                date_subset.c.income_at_exit > date_subset.c.demographics["income"].astext.cast(Float)))
+    return income.count()
