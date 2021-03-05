7544dba64dbcfe3bbfb226cbaa5b1973e5172d03
ik-okoro <ik.o.okoro@gmail.com>
2021-03-05 07:25:44 -0500

    Improved documentation/cleanup
    Adds summaries, description, tags, and commenting for most of the routes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 """
-Main file for routes
+Main app file for routes
 """
 import pandas as pd
 from fastapi import Depends, FastAPI, HTTPException, Path
@@ -13,11 +13,16 @@ from .crud import view_family, view_member, count_exits, avg_stay, income_increa
 from .visualizations import pie_exits, moving_average
 from .predict import predictor
 
+# Set app title and interactive docs for home route
 app = FastAPI(title='Family Promise DS API - Team B',
     docs_url='/')
-# app = FastAPI()
 
+
+# CORS security. Currently allows access from any site's call
+# Origins can be updated for only web's API when it's good to do so
+# See ReadME for more info
 app.add_middleware(CORSMiddleware, allow_origins = ["*"], allow_methods = ["*"], allow_headers = ["*"])
+
 
 # Dependency
 def get_db():
@@ -27,36 +32,87 @@ def get_db():
     finally:
         db.close()
 
+
 # Routes
-@app.get("/members/{member_id}")
+@app.get("/members/{member_id}", summary = "Find a member", tags = ["Records"])
 def read_member(member_id: int = Path(..., gt = 0), db: Session = Depends(get_db)):
+    """
+    Retrieves a member's details after passing in **member_id**.
+    If no such member in the database, returns 404 Error: Member not found in DB.
+    """
     db_member = view_member(db, member_id= member_id)
+
+    # Set error handling exception for member_id not in DB
     if db_member is None:
         raise HTTPException(status_code = 404, detail = "Member not found in DB")
+    
     return db_member
 
-@app.get("/families/{family_id}")
+
+@app.get("/families/{family_id}", summary = "Find a family", tags = ["Records"])
 def read_family(family_id: int = Path(..., gt= 0), db: Session = Depends(get_db)):
+    """
+    Retrieves a family's details after passing in their given **family_id**.
+    If no such family in the database, returns 404 Error: Family not found in DB.
+    """
     db_family = view_family(db, family_id = family_id)
+
+    # Set error handling exception for family_id not in DB
     if db_family is None:
         raise HTTPException(status_code= 404, detail = "Family not found in DB")
+    
     return db_family
 
 
-@app.get("/exits/{time_range}")
+@app.get("/exits/{time_range}", summary = "View exit percentages", tags = ["KPIs"])
 def all_exits(time_range: int = Path(..., gt = 0, le = 365), db: Session = Depends(get_db)):
-    permanents = count_exits(db, exit_type = 'Permanent Exit', time_range= time_range)
-    temps = count_exits(db, exit_type= 'Temporary Exit', time_range= time_range)
-    transitionals = count_exits(db, exit_type= 'Transitional Housing', time_range= time_range)
-    unknowns = count_exits(db, exit_type= 'Unknown/Other', time_range= time_range)
-    emergencies = count_exits(db, exit_type= 'Emergency Shelter', time_range= time_range)
-    exits = {"Permanent Exits" : permanents, "Emergency Shelter" : emergencies, "Temporary Exits" : temps, "Transitional Housing" : transitionals, "Unknown/Other" : unknowns}
-    return exits
+    """
+    Retrieve the breakdown of all exits in percentages over a date range.
+    Requires passing in a **time_range** to create the date range.
+        *time_range = today - start date (in days)*
+        time_range limit set as 0 < time_range <= 365
+    Returns percentage of each of these exit types:
+        - Permanent Exit
+        - Emergency Shelter
+        - Temporary Exit
+        - Transitional Housing
+        - Unknown/Other
+    """
+    exits = ["Unknown/Other", "Permanent Exit", "Emergency Shelter", "Temporary Exit", "Transitional Housing"]
 
-@app.get("/average_stay/{time_range}")
+    # Initialize exits_dict to return with zeros
+    exits_dict = {"Permanent Exits" : 0, "Emergency Shelter" : 0, "Temporary Exits" : 0, "Transitional Housing" : 0, "Unknown/Other" : 0}
+
+    # Calculate average for each exit type over date range
+    for key, each_exit in zip(exits_dict.keys(), each_exit):
+        exits_dict[key] = count_exits(db, exit_type = each_exit, time_range= time_range)
+
+    return exits_dict
+    # permanents = count_exits(db, exit_type = 'Permanent Exit', time_range= time_range)
+    # temps = count_exits(db, exit_type= 'Temporary Exit', time_range= time_range)
+    # transitionals = count_exits(db, exit_type= 'Transitional Housing', time_range= time_range)
+    # unknowns = count_exits(db, exit_type= 'Unknown/Other', time_range= time_range)
+    # emergencies = count_exits(db, exit_type= 'Emergency Shelter', time_range= time_range)
+    # exits = {"Permanent Exits" : permanents, "Emergency Shelter" : emergencies, "Temporary Exits" : temps, "Transitional Housing" : transitionals, "Unknown/Other" : unknowns}
+    # return exits
+
+
+@app.get("/average_stay/{time_range}", summary = "Average length of stay", tags = ["KPIs"])
 def average_stay(time_range: int = Path(..., gt = 0, le = 365), db: Session = Depends(get_db)):
-    stay_avg = avg_stay(db, time_range = time_range) #TODO: Find out how they want the avg rounded
+    """
+    View the average length of families' stay in the shelter over a date range.
+    Requires passing in a **time_range** to create the date range.
+        *time_range = today - start date (in days)*
+        time_range limit set as 0 < time_range <= 365
+    
+    Returned average is value rounded to an integer
+    """
+    # Calculate average over date range
+    stay_avg = avg_stay(db, time_range = time_range)
+    
+    # JSON format for web calls
     avg = {"Average Stay" : stay_avg}
+    
     return avg
 
 @app.get("/income/{time_range}")
@@ -82,6 +138,7 @@ def predict_exits(member_id: int = Path(..., gt = 0), db: Session = Depends(get_
     exit = predictor(df)
     return exit
  
+
 
 
 # Debugging
