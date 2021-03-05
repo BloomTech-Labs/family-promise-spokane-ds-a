e103ebbb854f63d6b9f5490ca24a014a7fc9e96c
ik-okoro <ik.o.okoro@gmail.com>
2021-03-05 06:24:57 -0500

    Improves documentation
    Adds more docstrings and comments for visualizations file

--- app/visualizations.py
+++ app/visualizations.py
@@ -1,7 +1,7 @@
 """
 File for generating plotly visualizations:
-    - Plot for the exit type percentages
-    - Plot for the 90 day moving averages of exit types
+    - Pie chart for the exit type percentages
+    - Line chart for the 90 day moving averages of exit types
 """
 
 import pandas as pd
@@ -12,12 +12,27 @@ from sqlalchemy.orm import Session
 from .crud import exit_date_subset, count_exits, today
 
 def pie_exits(db: Session, time_range: int):
+    """
+    Generates an interactive pie chart with plotly showing exit type percentages over a date range.
+
+    Returns: plot as a JSON object for the API.
+
+    Parameters:
+    db: Database session
+    time_range: n days to use for date subset
+    """
+    # Call date subset generator function for required n days
     date_subset = exit_date_subset(db=db, start_range=time_range)
+
+    # Create SQL query for exit types column that can be converted to dataframe
     query = db.query(date_subset.c.exit_destination)
     df = pd.read_sql(query.statement, query.session.bind)
+
     exits = ['Unkown/Other', 'Permanent Exit', 'Emergency Shelter', 'Temporary Exit', 'Transitional Housing']
-    # Make the plot of exit type to count of people to show how they are exiting
+
+    # Generate plotly pie chart then return json object
     fig = go.Figure([go.Pie(labels=exits, values=df['exit_destination'].value_counts())])
+    
     fig.update_layout(
         title={
             'text': "Exit Destinations",
@@ -32,19 +47,42 @@ def pie_exits(db: Session, time_range: int):
 
 
 def moving_average(db: Session, time_range: int):
+    """
+    Generates an interactive line chart with plotly showing exit type daily 90-day moving average over a date range.
+
+    Returns: plot as a JSON object for the API.
+
+    Parameters:
+    db: Database session
+    time_range: n days to use for date subset
+    """
     exits = ["Unknown/Other", "Permanent Exit", "Emergency Shelter", "Temporary Exit", "Transitional Housing"]
+
+    # Create date range with pandas for easier iteration and adding to dataframe
     daterange = pd.date_range(today - timedelta(days = time_range), today)
+
+    # Dictionary to hold each day's 90-day exit average
     averages_dict = {"Permanent Exit" : [], "Emergency Shelter" : [], "Temporary Exit" : [], "Transitional Housing" : [], "Unknown/Other" : []}
+
+    # Use the count_exits function to calculate averages
+    # Default time_range to 90 for 90-day averages
+    # Iterates over every day for each exit type
+    #TODO: O(n^2). Look into optimizing calculation
+
     for exit_type in exits:
         for day in daterange:
             avg = count_exits(db= db, exit_type= exit_type, time_range= 90, stop= day)
             averages_dict[exit_type].append(avg)
 
-
+    # Add each day to averages dictionary to create dataframe
     averages_dict.update({"Days" : daterange})
     avg_df = pd.DataFrame(averages_dict)
 
+    # Instantiate plotly figure
     fig = go.Figure()
+
+    # Add each exit type's line plot. 
+    # Colours are mapped to the same colours created by the pie chart
     fig.add_trace(go.Scatter(
         x=avg_df["Days"],
         y=avg_df["Permanent Exit"],
